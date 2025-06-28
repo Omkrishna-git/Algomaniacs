b8e12029c31dd6770ba7ad7941d7c6beb87322e6
Onkar Dhanaji Patil <148969149+Omkrishna-git@users.noreply.github.com>
2025-06-28 23:34:27 +0530

    Update A_Adjacent_Digit_Sums.cpp

--- A_Adjacent_Digit_Sums.cpp
+++ A_Adjacent_Digit_Sums.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
+
 
 #define int long long int
 #define uint unsigned long long
